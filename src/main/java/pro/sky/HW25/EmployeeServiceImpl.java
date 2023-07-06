78f6f78b9fdb7c4f5aac26cb5427c2e9980b9a93
ZU <zshaihova@gmail.com>
2023-07-06 22:32:46 +0300

    ДЗ сделана

---
+++ src/main/java/pro/sky/HW25/EmployeeServiceImpl.java
@@ -0,0 +1,59 @@
+package pro.sky.HW25;
+
+import org.springframework.stereotype.Service;
+import pro.sky.HW25.exceptions.BadParamsException;
+import pro.sky.HW25.exceptions.EmployeeAlreadyAddedException;
+import pro.sky.HW25.exceptions.EmployeeNotFoundException;
+import pro.sky.HW25.exceptions.EmployeeStorageIsFullException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private final List<Employee> employees;
+    private int size = 2;
+
+    public EmployeeServiceImpl() {
+        this.employees = new ArrayList<>(size);
+    }
+
+    public Employee addEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (firstName =="" || lastName == "") {
+            throw new BadParamsException();
+
+        }
+        if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException("этот сотрудник уже существует");
+        }
+        if (employees.size() > size) {
+            throw new EmployeeStorageIsFullException("лимит сотрудников превышен");
+        }
+        employees.add(employee);
+        return employee;
+    }
+
+    public Employee removeEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+
+        if (employees.contains(employee)) {
+            employees.remove(employee);
+            return employee;
+        }
+        throw new EmployeeNotFoundException("этот сотрудник отсутствует");
+    }
+
+    public Employee findEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employees.contains(employee)) {
+            return employee;
+        }
+        throw new EmployeeNotFoundException("этот сотрудник отсутствует");
+    }
+
+    public Collection<Employee> printAll() {
+        return new ArrayList<>(employees);
+    }
+}
